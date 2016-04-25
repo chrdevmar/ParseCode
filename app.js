4658fc484f3b00bd8dc2e90137d8128fc18eb58e
wangchonghoe <chris.martin.95@hotmail.com>
2016-04-25 19:34:51 +1000

    Setup shell and implemented views
    Setup shell and implemented views

---
+++ app.js
@@ -0,0 +1,48 @@
+//create module
+var parseCode = angular.module('parseCode', ['ngRoute']);
+
+//configure routes
+parseCode.config(function($routeProvider) {
+
+		//home
+		$routeProvider.when('/home', {
+		templateUrl : 'views/home.html',
+		controller  : 'homeController'
+		});
+
+		//me
+		$routeProvider.when('/me', {
+		templateUrl : 'views/me.html',
+		controller  : 'meController'
+		});
+
+		//projects
+		$routeProvider.when('/projects', {
+		templateUrl : 'views/projects.html',
+		controller  : 'projectController'
+		});
+
+		$routeProvider.otherwise({
+        redirectTo: '/home'
+      });
+});
+
+//home controller
+parseCode.controller('homeController', function($scope){
+	$scope.message = 'this is the home view';
+
+	$scope.sayHi = function(){
+		window.alert("hi");
+	}
+});
+
+//me controller
+parseCode.controller('meController', function($scope){
+	$scope.message = "this is the me view";
+});
+
+//projects controller
+parseCode.controller('projectController', function($scope){
+	$scope.message = "this is the projects view";
+});
+
